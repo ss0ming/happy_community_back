985aea462abd3f7dce3e3c8c2db13111bfa04afd
euniiiii <hee9920@naver.com>
2024-06-17 13:25:31 +0900

    Initial commit

---
+++ src/test/java/com/example/happy_community_back/HappyCommunityBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happy_community_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyCommunityBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
